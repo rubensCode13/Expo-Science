50f12212b54651bb57824d357427f3534250dfb6
Rubens <rubensletullier@icloud.com>
2024-12-27 15:05:08 -0500

    Update App.java
    J'ai amélioré et presque fini le code

--- App.java
+++ App.java
@@ -1,35 +1,28 @@
 
-import java.util.Scanner;
-
 public class App {
 
-    public static void main(String args[]) {
-        AnimalsofQuebec animals = new AnimalsofQuebec();
-        animals.animals();
-        String erreur = "Erreur. Veuillez recommencer";
+    public static void filter() {
 
+    }
+
+    public static void main(String args[]) {
+        String erreur = "Erreur. Veuillez recommencer";
+        AnimalsofQuebec animal = new AnimalsofQuebec();
+        
         try {
             AnimalType[] animalClass = AnimalType.values();
             for (int i = 0; i<animalClass.length; i++) {
                 System.out.println(i + " " + animalClass [i]);            
             }
-            Scanner keyboard = new Scanner(System.in);
-            System.out.println("Veuiller entrer le numéro associer à la classe que vous voulez rechercher");
-            int userInput = keyboard.nextInt();
-                
-            double enumLenght = animalClass.length;
-            if (userInput <= enumLenght && userInput >= 0) {
-
-            } else {
-                System.out.println("Erreur");
-                
-        } 
+            animal.animals();
+            
         } catch (Exception e) {
             System.out.println(erreur);
         }
     
 }
 }
+
 
 public enum AnimalType {
     
@@ -49,6 +42,10 @@ public class Animal {
     private String family;
     private boolean threatened;
 
+   public Animal () {
+    
+   }
+
     public Animal(String animalName, String latinName, String animalClass, String order, String family, boolean threatened) {
         this.animalName = animalName;
         this.latinName = latinName;
@@ -58,31 +55,63 @@ public class Animal {
         this.threatened = threatened;
     }
 
+    void display() {
+        System.out.println(animalName);
+    }
+    void filterMollusc() {
+       if (animalClass.equals("Bivalve")) {
+        display();
+       }
+    }
+    void filterInsect() {
+        if (animalClass.equals("Insecte")) {
+            display();
+        }
+    }
+    void filterFish() {
+        if (animalClass.equals("")) {
+            display();
+        }
+    }
+    void filterAmphibi() {
+        if (animalClass.equals("Mammifère")) {
+            display();
+        }
+    }
+    void filterBird() {
+        if (animalClass.equals("Aves")) {
+            display();
+        }
+    }
+
+    void filterMammal() {
+        if (animalClass.equals("Mammifère")) {
+            display();
+        }
+    }
+
 }
+
+import java.util.Scanner;
 
 class AnimalsofQuebec {
 
     public AnimalsofQuebec() {
     }
-    
+    public static void animals() {
+        Animal [] animals = {
+            new Animal("Anodonte du gaspareau", "Utterbackiana implicata", "Bivalve", null, null, true),
+            new Animal("Obovarie olivatre", "Obovaria olivaria", "Bivalve", null, null, true),
 
-	public static void animals() {
-        Animal[] animals = {
-        };
-        Animal [] mollusc = {
-            
-        };
-        Animal [] insect = {
             new Animal("Coccinelle à deux points", "Adalia bipunctata", "Insecte","Colleoptère", "Coccinellidae", false ),
             new Animal("Bourdon à tache rousse", "Bombus affinis", "Insecte", "Hymenoptère", "Apidae", true),      
             new Animal("Coccinelle à neuf points", "Coccinella novemnotata", "Insecte", "Coleoptère", "Coccinellidae", true),
             new Animal("Cuivré des marais salés", "Lycaena dospassosi", "Insecte", "Lepidoptère", "Lycaenidae", true),
             new Animal("Satyre fauve des Maritimes", "Coenonympha nipisiquit", "Insecte", "Lepidoptère", "Nymphalidae", true),
-        };
-        Animal [] fish = {
+
             new Animal("Alose savoureuse", "Alosa sapidissima", "Actinopterygii", "Clupeiforme", "Clupeidae", false),
             new Animal("Brochet vermiculé", "Esox americanus vermiculatus", "Actinopterygii", "Esociforme", "Esocidae", false),
-            new Animal("Chat-fou des rapides", "Noturus flavus", "Actinopteryg", "Siluriforme", "Ictaluridae", false),
+            new Animal("Chat-fou des rapides", "Noturus flavus", "Actinopterygii", "Siluriforme", "Ictaluridae", false),
             new Animal("Chevalier de rivière", "Moxostoma carinatum", "Actinopterygii", "Cypriniforme", "Catostomidae", false),
             new Animal("Éperlan arc-en-ciel", "Osmerus mordax", "Actinopterygii", "Osmeroidei", "Osmeridae", false),
             new Animal("Fouille-roche gris", "Percina copelandi", "Teleostei", "Perciforme", "Percidae", false),
@@ -94,13 +123,11 @@ class AnimalsofQuebec {
             new Animal("Dard de sable", "Ammocrypta pellucida", "Actinopterygii", "Perciforme", "Percidae", true),
             new Animal("Lamproie du Nord", "Ichthyomyzon fossor", "Cephalaspidomorphi", "Petromyzontiforme", "Petromyzontidae", true),
             new Animal("Raie tachetée", "Leucoraja ocellata", "Elasmobranchii", "Rajiforme", "Rajidae", true),
-        };
-        Animal [] amphibian = {
+
             new Animal("Salamandre pourpre","Gyrinophilus porphyriticus","Amphibien","Caudata","Plethodontidae", false),
             new Animal("Rainette faux-grillon de l'Ouest","Pseudacris triseriata","Amphibien","Anura","Hylidae", true),
             new Animal("Salamandre sombre des montagnes","Desmognathus ochrophaeus","Amphibien","Caudata","Plethodontidae", true),
-        };
-        Animal [] reptile = {
+
             new Animal("Tortue des bois","Glyptemys insculpta","Reptile","Testudine","Emydidae", false),
             new Animal("Tortue géographique","Graptemys geographica","Reptile","Testudine","Emydidae", false),
             new Animal("Couleuvre d'eau du Nord","Nerodia sipedon sipedon","Reptile","Squamata","Natrcidae", false),
@@ -111,8 +138,6 @@ class AnimalsofQuebec {
             new Animal("Tortue molle à épines","Apalone spinifera","Reptile","Testudine","Trionychidae", true),
             new Animal("Couleuvre brune","Storeria dekayi","Reptile","Squamata","Natricidae", true),
 
-        };
-        Animal [] bird = {
             new Animal("Aigle royal","Aquila chrysaetos","Aves","Accipitriforme","Accipitridae", false),
             new Animal("Arlequin plongeur","Histrionicus histrionicus","Aves","Anseriforme","Anatidae", false),
             new Animal("Engoulevent bois-pourris","Antrostomus vociferus","Aves","Caprimulgiform","Caprimulgidae", false),
@@ -136,8 +161,6 @@ class AnimalsofQuebec {
             new Animal("Sterne caspienne","Hydroprogne caspia","Aves","Charadriiform","Laridae", true),
             new Animal("Sterne de Dougall","Sterna dougallii","Aves","Charadriiform","Laridae", true),
 
-        };
-        Animal [] mammal = {
             new Animal("Caribou des bois, écotype forestier","Rangifer tarandus caribou","Mammifère","Artiodactyla","Cervidae", false),
             new Animal("Chauve-souris rousse de l'Est","Lasiurus borealis","Mammifère","Chiropetera","Vespertilionidae", false),
             new Animal("Ours blanc","Ursus maritimus","Mammifère","Carnivora","Ursidae", false),
@@ -147,13 +170,56 @@ class AnimalsofQuebec {
             new Animal("Caribou des bois, écotype montagnard","Rangifer tarandus caribou","Mammifère","Artiodactyla","Cervidae", true),
             new Animal("Chauve-souris nordique","Myotis septentrionalis","Mammifère","Chiroptera","Vespertilionidae", true),
             new Animal("Petite chauve-souris brune","Myotis lucifugus","Mammifère","Chiroptera","Vespertilionidae", true),
-            new Animal("Pipistrelle de l'Est","Perimyotis subflavus","Mammifère","Chiroptera","Vespertilionidae", true),
-        };
-               
-    }
-}
-
-	
-}
-}
-
+            new Animal("Pipistrelle de l'Est","Perimyotis subflavus","Mammifère","Chiroptera","Vespertilionidae", true),            
+        };  
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Veuiller entrer le numéro associer au type d'animal que vous voulez rechercher");
+        int userInput = keyboard.nextInt();
+        AnimalType[] animalClass = AnimalType.values();
+        int enumLenght = animalClass.length;
+        int i = 0;
+        if (userInput <= enumLenght && userInput >= 0) {
+                switch (userInput) {
+                    case 0: 
+                        System.out.println("Voici les mollusques: ");
+                        for (Animal a : animals) {
+                            a.filterMollusc();
+                        }
+                        break;
+                    case 1:
+                        System.out.println("Voici les insectes: ");
+                        for (Animal a : animals) {
+                            a.filterInsect();
+                        }
+                        break;
+                    case 2:
+                    System.out.println("Voici les poissons: ");
+                    for (Animal a : animals) {
+                        a.filterFish();
+                    }
+                        break;
+                    case 3:
+                    System.out.println("Voici les amphibiens: ");
+                    for (Animal a : animals) {
+                        a.filterAmphibi();
+                    }
+                        break;
+                    case 4:
+                    System.out.println("Voici les oiseaux: ");
+                    for (Animal a : animals) {
+                        a.filterBird();
+                    }
+                        break;
+                    case 5:
+                    System.out.println("Voici les mammifères: ");
+                    for (Animal a : animals) {
+                        a.filterMammal();
+                    }
+                        break; 
+                }
+                
+        } else {
+                System.out.println("Erreur");
+            }
+            // System.out.println(animals.length);
+                
